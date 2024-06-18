13a6417b0a8e9ef913a7c77940bd297ddd1d6e70
Ganakim <espene.cornwall@gmail.com>
2024-06-18 13:27:57 -0600

    Initial Commit

---
+++ c# 2015/Day 4.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+string input = "ckczppom";
+int output = -1;
+string hash;
+
+do
+{
+  output++;
+  byte[] bytes = Encoding.ASCII.GetBytes(input + output);
+  byte[] hash = MD5.Create().ComputeHash(bytes);
+
+  hash = string.Join("", hash.Select(a => a.ToString("x2")));
+} while (!hash.StartsWith("000000"));
+
+Console.WriteLine(output);
